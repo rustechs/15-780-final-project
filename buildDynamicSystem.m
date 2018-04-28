c92379810f69b993b0dd64b34f3de16543b2b79d
rustechs <rus.tech.studio@gmail.com>
2018-04-28 16:06:12 -0400

    working on matlab side of things

--- buildDynamicSystem.m
+++ buildDynamicSystem.m
@@ -1,7 +1,9 @@
 function buildDynamicSystem(sysName)
 
     % Parameters
-    T_sample = 1e3; % [Hz]
+    tPeriod = 1e3; % [Hz]
+    tSamples = 0:tPeriod:10; % [sec]
+    
     mu = []; % TODO
     Sigma = []; % TODO
 
@@ -11,6 +13,14 @@ function buildDynamicSystem(sysName)
 
     f = @(t,x) A*x;
     c = @(x) C*x;
+    
+    % Initial condition
+    x0 = []; % TODO 
+    y0 = c(x0);
+    
+    % State and output space dimensions
+    n = length(x0);
+    m = length(y0);
 
     % Save system specification into mat file
     save(['configs/' sysName])
--- genMeasurementData.m
+++ genMeasurementData.m
@@ -3,8 +3,35 @@ function genMeasurementData(sysName)
     % Load system specifications into a struct
     sys = load(sysName);
     
+    % Save all generated data to a struct
+    data = struct([]);
     
+    % Generate true system data
+    [data.t,data.x] = ode45(odeFcn,sys.tSamples,sys.x0);
     
-    % Save generated measurements to data folder
-
+    data.yTrue = zeros(length(data.t),sys.m);
+    for i = 1:length(data.t)
+        data.yTrue(i,:) = sys.c(data.x(i,:));
+    end
+    
+    % Generate noisy measurements
+    rng('shuffle');
+    R = chol(sys.Sigma);
+    data.yNoisy = data.yTrue + (repmat(sys.mu,length(data.t),1) + randn(size(data.yTrue))*R);
+    
+    % Save generated measurements as raw text files in data folder. Text
+    % file names match the name of the config file from which they were
+    % generated.
+    
+    save(['data/' sysName '.data'],'-struct','data','-ascii')
+    
+    % Plot some stuff
+    figure
+    hold on;
+    plot(data.t,data.yTrue,'-');
+    plot(data.t,data.yNoisy,'--');
+    box on;
+    grid minor;
+    xlabel('time');
+    ylabel('outputs');
 end
