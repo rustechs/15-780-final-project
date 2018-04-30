47a3a657982a2df2eb609cd8c623bb4b19888dd0
rustechs <rus.tech.studio@gmail.com>
2018-04-30 17:42:57 -0400

    fixed data saving not working correctly

--- genMeasurementData.m
+++ genMeasurementData.m
@@ -2,34 +2,33 @@ function genMeasurementData(sysName)
 
     % Load system specifications into a struct
     sys = load(['configs/' sysName]);
-    
-    % Save all generated data to a struct
-    data = struct('t',[],'x',[],'yTrue',[]);
-    
+
     % Generate true system data
-    [data.t,data.x] = ode45(sys.f,sys.tSamples,sys.x0);
+    [t,x] = ode45(sys.f,sys.tSamples,sys.x0);
     
-    data.yTrue = zeros(length(data.t),sys.m);
-    for i = 1:length(data.t)
-        data.yTrue(i,:) = sys.c(data.x(i,:));
+    yTrue = zeros(length(t),sys.m);
+    for i = 1:length(t)
+        yTrue(i,:) = sys.c(x(i,:));
     end
     
     % Generate noisy measurements
     rng('shuffle');
     R = chol(sys.Sigma);
-    data.yNoisy = data.yTrue + (repmat(sys.mu,length(data.t),1) + randn(size(data.yTrue))*R);
+    yNoisy = yTrue + (repmat(sys.mu,length(t),1) + randn(size(yTrue))*R);
     
     % Save generated measurements as raw text files in data folder. Text
     % file names match the name of the config file from which they were
     % generated.
     
-    save(['data/' sysName '.data'],'-struct','data','-ascii')
+    saveMat = [t x yTrue yNoisy];
+    
+    dlmwrite(['data/' sysName '.data'],saveMat,'\t');
     
     % Plot some stuff
     figure
     hold on;
-    plot(data.t,data.yNoisy,'.');
-    plot(data.t,data.yTrue,'-','LineWidth',2);
+    plot(t,yNoisy,'.');
+    plot(t,yTrue,'-','LineWidth',2);
     box on;
     grid minor;
     xlabel('time');
