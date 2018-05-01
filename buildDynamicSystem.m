6a3ccb26866ce3d018d61ae1d8e8eb1d8e3f0dd7
rustechs <rus.tech.studio@gmail.com>
2018-05-01 16:28:02 -0400

    fixed matlab data output format

--- buildDynamicSystem.m
+++ buildDynamicSystem.m
@@ -19,22 +19,34 @@ Sigma = 0.1;
 % 
 % A = [0 1; -k/m -b/m];
 % C = [1 0];
-% 
+% B = [];
+
 % f = @(t,x) A*reshape(x,length(x),1);
 % c = @(x) C*reshape(x,length(x),1);
 
 % Nonlinear Example -- 2nd Order Model of a Sync Power Machine
 % See slide 39 of http://www.dt.fee.unicamp.br/~geromel/non_second.pdf
-f = @(t,x) [x(2); 0.5-sin(x(1))];
+%
+% xDot = f(t,x) + g(t,x)*u(t)
+% y = c(x)
+
+f1 = @(t,x) [x(2); 0.5-sin(x(1))];
 c = @(x) x;
+g = @(t,x) zeros(2,3);
+
+f = @(t,x,u) f1(t,x) + g(t,x)*reshape(u,length(u),1);
 
 %% Initial condition
 x0 = [1 -1]; % TODO 
 y0 = c(x0);
 
+%% Inputs
+u = @(t) ones(1,3);
+
 %% State and output space dimensions
-n = length(x0);
-m = length(y0);
+n = length(x0); % state-space dim
+m = length(y0); % output-space dim
+r = length(u(0)); % input-space dim
 
 %% Save system specification into mat file
 save(['configs/' sysName])
--- genMeasurementData.m
+++ genMeasurementData.m
@@ -1,14 +1,22 @@
-function genMeasurementData(sysName)
+function genMeasurementData(sysName,approxDegree)
+
+    if nargin < 2
+        approxDegree = 5;
+    end
 
     % Load system specifications into a struct
     sys = load(['configs/' sysName]);
 
     % Generate true system data
-    [t,x] = ode45(sys.f,sys.tSamples,sys.x0);
+    [t,x] = ode45(@(t,x) sys.f(t,x,sys.u(t)),sys.tSamples,sys.x0);
     
     yTrue = zeros(length(t),sys.m);
+    u = zeros(length(t),sys.r);
+    xDot = zeros(length(t),sys.n);
     for i = 1:length(t)
+        u(i,:) = sys.u(t(i));
         yTrue(i,:) = sys.c(x(i,:));
+        xDot(i,:) = sys.f(t(i),x(i,:),u(i,:));
     end
     
     % Generate noisy measurements
@@ -20,9 +28,11 @@ function genMeasurementData(sysName)
     % file names match the name of the config file from which they were
     % generated.
     
-    saveMat = [t x yTrue yNoisy];
+    infoMat = [sys.r sys.n sys.n sys.m approxDegree];
+    saveMat = [u x xDot yTrue yNoisy];
     
-    dlmwrite(['data/' sysName '.data'],saveMat,'\t');
+    dlmwrite(['data/' sysName '.data'],infoMat,'delimiter','\t');
+    dlmwrite(['data/' sysName '.data'],saveMat,'-append','delimiter','\t');
     
     % Plot some stuff
     figure
